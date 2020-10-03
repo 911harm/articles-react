03efd48b2ed00069179272c703cf6ce98b64e2a6
911harm <911harm@gmail.com>
2020-10-03 12:26:41 -0500

    Updates

---
+++ precache-manifest.f0bd03a8d4ed8adda365ec02c70fcf8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf34bb2997e3089eda76fd8c39f7eaf",
+    "url": "/articles-react/index.html"
+  },
+  {
+    "revision": "b9856d0d46122b283cc4",
+    "url": "/articles-react/static/css/main.ddd589e3.chunk.css"
+  },
+  {
+    "revision": "d335aadd2fb57e00355e",
+    "url": "/articles-react/static/js/2.3711d3b1.chunk.js"
+  },
+  {
+    "revision": "d5e06592ca90ebc48234022e463e66f4",
+    "url": "/articles-react/static/js/2.3711d3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9856d0d46122b283cc4",
+    "url": "/articles-react/static/js/main.cd1de4bb.chunk.js"
+  },
+  {
+    "revision": "04a20f29979bc76a596a",
+    "url": "/articles-react/static/js/runtime-main.984fa27a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/articles-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
